fca9a7df6373752e94a5a9748ecfbfed52161002
frederic forjan <fforjan@free.fr>
2020-03-06 10:21:34 -0800

    adding simple addin

---
+++ AddinImpl/Startup.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Addin
+{
+   using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public  class Startup : ServiceInterfaces.IAddInStartup
+    {
+        public string Name => "Addin Implementation";
+
+        public void ConfigureServices(IConfiguration configuration, IServiceCollection services) {             
+        }
+        
+    }   
+}
